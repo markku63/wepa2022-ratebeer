3b81469fe72956d3ca9509bd2a6adf801a1bcc46
Markku Kolkka <markku.kolkka@iki.fi>
2022-10-06 13:07:32 +0300

    Exercise 3.4

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
   root "breweries#index"
 
   get 'kaikki_bisset', to: 'beers#index'
-  #get 'ratings', to: 'ratings#index'
-  #get 'ratings/new', to: 'ratings#new'
-  #post 'ratings', to: 'ratings#create'
+  # get 'ratings', to: 'ratings#index'
+  # get 'ratings/new', to: 'ratings#new'
+  # post 'ratings', to: 'ratings#create'
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   resource :session, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
