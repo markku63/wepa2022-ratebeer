7bed9b2f0f12773af1aa732555c437b062b75093
Markku Kolkka <markku.kolkka@iki.fi>
2022-10-07 13:31:35 +0300

    Exercise 3.12

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true,
                        length: { in: 3..30 }
+  validates :password, length: { minimum: 4 },
+                       format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+                                 message: "not complex enough" }
 
   has_many :ratings
   has_many :beers, through: :ratings
