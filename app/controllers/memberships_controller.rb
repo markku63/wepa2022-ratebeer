61f7e1602332a5edba31a94d92a023c8790591ba
Markku Kolkka <markku.kolkka@iki.fi>
2022-10-11 13:40:17 +0300

    Exercise 3.9

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,14 @@
+class MembershipsController < ApplicationController
+  def index
+    @memberships = Membership.all
+  end
+
+  def new
+    @membership = Membership.new
+    @beer_clubs = BeerClub.all
+  end
+
+  def create
+    raise
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,11 @@ Rails.application.routes.draw do
   # get 'ratings/new', to: 'ratings#new'
   # post 'ratings', to: 'ratings#create'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resources :memberships
 
   resource :session, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
+  get 'join', to: 'memberships#new'
 end
