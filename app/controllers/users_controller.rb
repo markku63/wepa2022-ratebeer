15342073895e1ca15e43faa9e92c761cc58fd7a4
Markku Kolkka <markku.kolkka@iki.fi>
2022-10-20 13:17:49 +0300

    Exercise 3.14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
-      if current_user == @user.id && @user.update(user_params)
+      if current_user == @user && @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -49,10 +49,13 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy if current_user == @user.id
+    @user.destroy if current_user == @user
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html {
+        session[:user_id] = nil
+        redirect_to :root, notice: "User was successfully destroyed."
+      }
       format.json { head :no_content }
     end
   end
