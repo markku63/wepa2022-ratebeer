b08f194ad5b77491cee401fd28f66112327c4c5a
Markku Kolkka <markku.kolkka@iki.fi>
2022-11-17 10:58:44 +0200

    Exercise 4.15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
 
     User.find(session[:user_id])
   end
+
+  def ensure_that_signed_in
+    redirect_to signin_path, notice: 'you should be signed in' unless current_user
+  end
 end
