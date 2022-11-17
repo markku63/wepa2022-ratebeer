0a1b5b064e28411031f4ebbd01d20370e5ab7922
Markku Kolkka <markku.kolkka@iki.fi>
2022-11-17 13:32:47 +0200

    Before exercise 5.2

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,20 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+
+    places = response.parsed_response['bmp_locations']['location']
+
+    return [] if places.is_a?(Hash) && places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do |place|
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    "455e455d1210d26f97a1815f9344e2f0"
+  end
+end
