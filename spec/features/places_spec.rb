59bba0e951c748805238a443c0441265c367c6d6
Markku Kolkka <markku.kolkka@iki.fi>
2022-11-17 13:54:43 +0200

    Exercise 5.2 started

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,22 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if multiple are returned, all are shown on the page" do
+    allow(BeermappingApi).to receive(:places_in).with("tampere").and_return(
+      [Place.new(name: "Salhojankadun Pub", id: 1),
+       Place.new(name: "Plevna", id: 2)]
+    )
+
+    visit places_path
+    fill_in('city', with: 'tampere')
+    click_button "Search"
+
+    expect(page).to have_content "Plevna"
+    expect(page).to have_content "Salhojankadun Pub"
+  end
+
+  it "if none are returned, a notice is shown" do
+    visit places_path
+  end
 end
