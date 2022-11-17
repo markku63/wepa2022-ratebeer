89f473ffb0c79e60642b43863b2761471b5ab698
Markku Kolkka <markku.kolkka@iki.fi>
2022-11-17 11:24:53 +0200

    Exercise 4.16

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -6,24 +6,27 @@ describe "Beers page" do
   before :each do
     FactoryBot.create :user
     FactoryBot.create :brewery
+    sign_in(username: "Pekka", password: "Foobar1")
   end
 
-  it "should allow creating with valid name" do
-    visit beers_path
-    click_link "New beer"
-    fill_in('beer_name', with: 'Beer1')
-    click_button('Create Beer')
+  describe "when signed in" do
+    it "should allow creating with valid name" do
+      visit beers_path
+      click_link "New beer"
+      fill_in('beer_name', with: 'Beer1')
+      click_button('Create Beer')
 
-    expect(page).to have_content 'Beer was successfully created'
-    expect(page).to have_content 'Beer1'
-  end
+      expect(page).to have_content 'Beer was successfully created'
+      expect(page).to have_content 'Beer1'
+    end
 
-  it "should prevent creating with invalid name" do
-    visit beers_path
-    click_link "New beer"
-    click_button('Create Beer')
+    it "should prevent creating with invalid name" do
+      visit beers_path
+      click_link "New beer"
+      click_button('Create Beer')
 
-    expect(page).to have_content 'error prohibited this beer from being saved'
-    expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content 'error prohibited this beer from being saved'
+      expect(page).to have_content "Name can't be blank"
+    end
   end
 end
