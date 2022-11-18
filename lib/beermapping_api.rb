4b5726cbee16b0d6dc5117b3fbe87dc9572031ee
Markku Kolkka <markku.kolkka@iki.fi>
2022-11-18 13:03:27 +0200

    Exercise 5.4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,9 +1,14 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+
+    Rails.cache.fetch(city, expires_in: 1.day) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-
     places = response.parsed_response['bmp_locations']['location']
 
     return [] if places.is_a?(Hash) && places['id'].nil?
