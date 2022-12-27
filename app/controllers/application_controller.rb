d663d7622e1ab1344af305d36f02afab3fef9c54
Markku Kolkka <markku.kolkka@iki.fi>
2022-12-27 18:52:02 +0200

    Finished with Week 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def ensure_that_signed_in
     redirect_to signin_path, notice: 'you should be signed in' unless current_user
   end
+
+  def ensure_that_admin
+    redirect_to breweries_path, notice: 'yuou should be an admin' unless current_user.admin
+  end
 end
--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -5,7 +5,7 @@ describe "Breweries page" do
     visit breweries_path
 
     expect(page).to have_content 'Breweries'
-    expect(page).to have_content 'Number of breweries: 0'
+    expect(page).to have_content 'Number of active breweries: 0'
   end
 
   describe "when breweries exist" do
@@ -13,14 +13,14 @@ describe "Breweries page" do
       @breweries = ["Koff", "Karjala", "Schenkerla"]
       year = 1896
       @breweries.each do |brewery_name|
-        FactoryBot.create(:brewery, name: brewery_name, year: year+=1)
+        FactoryBot.create(:brewery, name: brewery_name, year: year+=1, active: true)
       end
 
       visit breweries_path
     end
 
     it "lists the existing breweries and their total number" do
-      expect(page).to have_content "Number of breweries: #{@breweries.count}"
+      expect(page).to have_content "Number of active breweries: #{@breweries.count}"
 
       @breweries.each do |brewery_name|
         expect(page).to have_content brewery_name
